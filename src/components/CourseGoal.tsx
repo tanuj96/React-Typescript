31c6b3b712c71bf1f8212c8c23fe37b769211438
tanuj96 <tanujmittal96@gmail.com>
2025-08-13 18:34:03 +0530

    ts props type

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,14 @@
-export default function CourseGoal() {
+type CourseGoalProps = {
+  title: string;
+  description: string;
+};
+
+export default function CourseGoal({ title, description }: CourseGoalProps) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
       <button>Delete</button>
     </article>
